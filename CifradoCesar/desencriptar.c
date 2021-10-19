6972cfa0165da2f93209d2f170447a5f81ffb2e6
Frey <31017769+FreyNazara@users.noreply.github.com>
2021-10-18 20:26:14 -0500

    Update desencriptar.c

--- CifradoCesar/desencriptar.c
+++ CifradoCesar/desencriptar.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include "desencriptar.h"
 
+
 int main()
 {
-char mensaje[100];
+char mensaje[100]; 
 int llave;
 printf("Ingresa el mensaje a descifrar: ");
 fgets(mensaje, sizeof(mensaje), stdin);
@@ -15,27 +16,38 @@ printf("El texto descifrado es: %s\n", result);
 return 0;
 }
 
+/*
+*Función que desencripta una cadena recorriéndola n posiciones en el alfabeto utilizando un char no-signado para no salir del alfabeto
+*@param msg, el mensaje que pasamos desde el input del usuario
+*@param k, la llave de corrimiento
+*@return msg, el mensaje desencriptado
+*/
 char * desencriptar(char* msg, int k) 
-    {
+{
     unsigned char caracter;
     int llave = k;
     int i;
-    for(i = 0; msg[i] != '\0'; ++i){
-    unsigned caracter = msg[i];
-    if(caracter >= 'a' && caracter <= 'z'){
-    caracter = caracter - llave;
-    if(caracter < 'a'){
-    caracter = caracter + 'z' - 'a' + 1;
-    }
-    msg[i] = caracter;
-    }
-    else if(caracter >= 'A' && caracter <= 'Z'){
-    caracter = caracter - llave;
-    if(caracter < 'A'){
-    caracter = caracter + 'Z' - 'A' + 1;
-    }
-    msg[i] = caracter;
-    }
+    for(i = 0; msg[i] != '\0'; ++i)
+    {
+        unsigned caracter = msg[i];
+        if(caracter >= 'a' && caracter <= 'z')
+        {
+            caracter = caracter - llave;
+            if(caracter < 'a')
+            {
+                caracter = caracter + 'z' - 'a' + 1;
+            }
+            msg[i] = caracter;
+        }
+        else if(caracter >= 'A' && caracter <= 'Z')
+        {
+            caracter = caracter - llave;
+            if(caracter < 'A')
+            {
+                caracter = caracter + 'Z' - 'A' + 1;
+            }
+        msg[i] = caracter;
+        }
     }
     return msg;
 }
